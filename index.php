7c045541efb54aefd7804dccb9b55994c217ab3b
MrArcano <salvatore-dario@hotmail.it>
2023-10-26 14:06:26 +0200

    [add]: stamp data with php

--- index.php
+++ index.php
@@ -60,22 +60,26 @@
     <table class="table">
       <thead>
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Parking</th>
+          <th scope="col">Vote</th>
+          <th scope="col">Distance to center</th>
         </tr>
       </thead>
       <tbody>
+        <?php foreach ($hotels as $key => $hotel):?>
         <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
+          <td><?php echo $hotel["name"] ?></td>
+          <td><?php echo $hotel["description"] ?></td>
+          <td><?php echo $hotel["parking"] ? "Si" : "No" ?></td>
+          <td><?php echo $hotel["vote"] ?></td>
+          <td><?php echo $hotel["distance_to_center"] ?></td>
         </tr>
+        <?php endforeach ?>
       </tbody>
     </table>
-    
+
   </div>
 </body>
 </html>
